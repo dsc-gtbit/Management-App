1767bb34b235203086804c29505b8203839db913
Aryan  Chaudhary <aryanchaudhary@Aryans-MacBook-Air.local>
2024-03-13 00:29:09 +0530

    Basic skeleton

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -5,7 +5,7 @@ class AppRouter{
   Route onGenerateRoute(RouteSettings routeSettings){
     switch (routeSettings.name){
       default:
-      return MaterialPageRoute(builder: (_) => const ());
+      return MaterialPageRoute(builder: (_) => const Center());
     }
 
   }
