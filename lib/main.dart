315b3cb9e8d5f4a6c6ee176c0c9fd0dd639b106b
Aryan  Chaudhary <aryanchaudhary@Aryans-MacBook-Air.local>
2024-03-13 00:23:36 +0530

    Basic skeleton

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'route/routes.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [],
+      child: MaterialApp(
+        onGenerateRoute: (settings) => AppRouter().onGenerateRoute(settings),
+      ),
+    );
+  }
+}
