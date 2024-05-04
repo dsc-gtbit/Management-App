a1bd1aa1b554f1ce8cbff693f9c7e74b3f823533
Pratyush Singh <pratyushsingh2005@gmail.com>
2024-05-05 00:32:01 +0530

    Fixed Login Page
    Added Calibri Fonts,College name, fixed border radius and padding

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         )
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         initialRoute: "/login",
         onGenerateRoute: (settings) => AppRouter().onGenerateRoute(settings),
       ),
--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:responsive_sizer/responsive_sizer.dart';
 
@@ -25,7 +24,30 @@ class _LoginScreenState extends State<LoginScreen> {
                 decoration: const BoxDecoration(color: Colors.white),
               ),
               Align(
-                  alignment: const Alignment(0, -0.6),
+                  alignment: const Alignment(0, -0.80),
+                  child: Text(
+                    textAlign: TextAlign.center,
+                    'Guru Tegh Bahadur\nInstitute of Technology',
+                    //textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontFamily: 'Calibri',
+                      shadows: const [
+                        Shadow(
+                          color: Color.fromARGB(
+                              48, 0, 0, 0), // Choose the color of the shadow
+                          blurRadius:
+                              5.0, // Adjust the blur radius for the shadow effect
+                          offset: Offset(2.0,
+                              2.0), // Set the horizontal and vertical offset for the shadow
+                        ),
+                      ],
+                      color: const Color.fromRGBO(0, 0, 0, 1),
+                      fontSize: 23.sp,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+              Align(
+                  alignment: const Alignment(0, -0.45),
                   child: Container(
                     height: 250,
                     width: 250,
@@ -39,11 +61,12 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                   )),
               Align(
-                  alignment: const Alignment(0, -0.13),
+                  alignment: const Alignment(0, -0.0),
                   child: Text(
                     'Welcome !',
                     //textAlign: TextAlign.left,
-                    style: GoogleFonts.openSans(
+                    style: TextStyle(
+                      fontFamily: 'Calibri',
                       shadows: const [
                         Shadow(
                           color: Color.fromARGB(
@@ -56,15 +79,26 @@ class _LoginScreenState extends State<LoginScreen> {
                       ],
                       color: const Color.fromRGBO(0, 0, 0, 1),
                       fontSize: 23.sp,
-                      fontWeight: FontWeight.w500,
+                      fontWeight: FontWeight.bold,
                     ),
                   )),
 
               Align(
-                  alignment: Alignment(0, 3.6.sp),
-                  child: SizedBox(
-                      height: 7.h,
-                      //width: 50.w,
+                  alignment: const Alignment(0, 0.85),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color.fromARGB(255, 192, 191, 191)
+                                .withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 20,
+                            offset: const Offset(5, -10),
+                          ),
+                        ],
+                      ),
+                      //height: 9.h,
+                      //width: 200,
                       child: Stack(children: <Widget>[
                         ElevatedButton(
                             style: ButtonStyle(
@@ -72,24 +106,25 @@ class _LoginScreenState extends State<LoginScreen> {
                                     MaterialStateProperty.all<Color>(
                                         Colors.black),
                                 padding: MaterialStateProperty.all<EdgeInsets>(
-                                    EdgeInsets.all(10.sp)),
+                                    EdgeInsets.all(7.sp)),
                                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(24.0),
+                                    borderRadius: BorderRadius.circular(8.0),
                                     side: const BorderSide(
                                         color: Color.fromARGB(255, 0, 0, 0))))),
                             onPressed: () {
                               debugPrintStack();
                             },
-                            child: Text(
-                                "                  Log In →                 ",
+                            child: Text("         Log In          ",
                                 style: TextStyle(
-                                    fontSize: 18.sp,
-                                    color:
-                                        const Color.fromARGB(255, 255, 255, 255)))),
+                                    fontFamily: 'Calibri',
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 22.sp,
+                                    color: const Color.fromARGB(
+                                        255, 255, 255, 255)))),
                       ]))),
 
               Align(
-                  alignment: const Alignment(0, 0.1),
+                  alignment: const Alignment(0, 0.2),
                   child: Container(
                       height: 55,
                       decoration: BoxDecoration(
@@ -101,19 +136,21 @@ class _LoginScreenState extends State<LoginScreen> {
 
                         //),
                       ),
-                      width: 300,
+                      width: 350,
                       child: const Stack(children: <Widget>[
                         TextField(
                           style: TextStyle(color: Colors.black),
                           decoration: InputDecoration(
+                            contentPadding: EdgeInsets.all(20),
                             suffixIcon: Icon(Icons.email_outlined),
                             filled: true,
                             fillColor: Color.fromARGB(17, 14, 140, 243),
                             border: OutlineInputBorder(
                                 borderRadius:
-                                    BorderRadius.all(Radius.circular(30))),
-                            labelText: '  Email',
+                                    BorderRadius.all(Radius.circular(8))),
+                            labelText: 'Email',
                             labelStyle: TextStyle(
+                              fontFamily: 'Calibri',
                               color: Colors.black,
                             ),
                             focusColor: Color.fromARGB(255, 167, 21, 21),
@@ -121,39 +158,43 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                       ]))),
               Align(
-                  alignment: Alignment(0, 1.8.sp),
+                  alignment: const Alignment(0, 0.35),
                   child: Container(
                       height: 55,
                       decoration: const BoxDecoration(),
-                      width: 300,
-                      child:  Stack(children: <Widget>[
+                      width: 350,
+                      child: Stack(children: <Widget>[
                         TextField(
                           obscureText: _isObscure,
                           style: const TextStyle(
-                              color: Colors.black, fontFamily: 'Inter'),
+                              color: Colors.black, fontFamily: 'Calibri'),
                           decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.all(20),
                               suffixIcon: IconButton(
-                    icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _isObscure = !_isObscure;
-                      });
-                    }),
+                                  icon: Icon(_isObscure
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: () {
+                                    setState(() {
+                                      _isObscure = !_isObscure;
+                                    });
+                                  }),
                               filled: true,
                               fillColor: const Color.fromARGB(17, 14, 140, 243),
                               border: const OutlineInputBorder(
                                   borderRadius:
-                                      BorderRadius.all(Radius.circular(30))),
+                                      BorderRadius.all(Radius.circular(8))),
                               focusColor: Colors.black,
-                              labelText: '  Password',
-                              labelStyle: const TextStyle(color: Colors.black)),
+                              labelText: 'Password',
+                              labelStyle: const TextStyle(
+                                  color: Colors.black, fontFamily: 'Calibri')),
                         ),
                       ]))),
               Align(
-                alignment: Alignment(0, 2.2.sp),
+                alignment: const Alignment(0, 0.45),
+
                 child: CheckboxListTile(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 100),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 27.sp),
                   activeColor: Colors.blue,
                   selectedTileColor: Colors.black,
                   title: const Text("Remember Me"),
