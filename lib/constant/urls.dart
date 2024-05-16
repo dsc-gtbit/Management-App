63ea66af230541252b98b301f971e163eba25e7e
anshd258 <anshd258@gmail.com>
2024-05-17 01:46:51 +0530

    @aryan_0813 changes in provider for yours refrence

---
+++ lib/constant/urls.dart
@@ -0,0 +1,2 @@
+const  baseUrl =
+      "https://script.google.com/macros/s/AKfycbw0Bq-gGvtsLurpEZva3iJ414KFnmBZJhwtjMXFRqAy75xijGv9-h3tfh5NDaT0bEvQ/exec";
--- lib/provider/Getsalprovider.dart
+++ lib/provider/Getsalprovider.dart
@@ -1,28 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:salary_slip/constant/urls.dart';
 import 'package:salary_slip/models/api_data.dart';
 import '../screens/api_response_screen.dart';
+import 'package:http/http.dart' as http;
 
 class GetSal with ChangeNotifier {
-  final APIResponseScreenState _apiResponseScreen = APIResponseScreenState(); // Initialize an instance of APIResponseScreen
+  final APIResponseScreenState _apiResponseScreen = APIResponseScreenState();
 
   bool isLoading = false;
   List<Employee> _employee = [];
   List<Employee> get employeesFuture => _employee;
 
-  Future<void> getAllTodos() async {
+  Future<List<Employee>> getAllTodos() async {
     isLoading = true;
     notifyListeners();
 
     try {
-      final response = await _apiResponseScreen.getEmployees(); // Call the getEmployees() method correctly
-      _employee = response;
+      var url = Uri.parse(baseUrl);
+      final response =
+          await http.get(url, headers: {"Content-Type": "application/json"});
+      final List body = json.decode(response.body)['data'];
+
+      _employee = body.map<Employee>((e) => Employee.fromJson(e)).toList();
+      isLoading = false;
+      notifyListeners();
+      return _employee;
     } catch (e) {
       // Handle error
       print(e);
+      isLoading = false;
+      notifyListeners();
+      rethrow;
     }
-
-    isLoading = false;
-    notifyListeners();
   }
 }
-
