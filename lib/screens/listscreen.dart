1879cdf5cdfce1971e04f0afe2d8e18a16ced1db
Tanu Jha <tanujha629@gmail.com>
2024-05-19 19:22:27 +0530

    changed theme

--- lib/screens/listscreen.dart
+++ lib/screens/listscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ListScreen extends StatelessWidget {
   static const listScreenRoute = "/listscreen";
@@ -14,23 +15,36 @@ class ListScreen extends StatelessWidget {
           child: ListView.builder(
             itemCount: 10, 
             itemBuilder: (context, index) {
-              return Card(
-                color: Color.fromARGB(255, 203, 198, 198),
-                child: ListTile(
-                  title: Text(
-                    'Title ${index + 1}',
-                    style: const TextStyle(color: Colors.black),
-                  ),
-                  subtitle: Text(
-                    'Description ${index + 1}',
-                    style: const TextStyle(color: Colors.black),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.download),
-                    color: Colors.black,
-                    onPressed: () {
-                      // download functionality
-                    },
+              return SizedBox(
+                height: 100,
+                child: Card(
+                  color: Colors.white,
+                  child: ListTile(
+                    title: Text(
+                      'Title ${index + 1}',
+                      style: GoogleFonts.lato( 
+                          textStyle: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 18.0, 
+                          ),
+                        ),
+                    ),
+                    subtitle: Text(
+                      'Description ${index + 1}',
+                      style: GoogleFonts.lato( 
+                          textStyle: const TextStyle(
+                            color: Colors.black54,
+                            fontSize: 14.0, 
+                          ),
+                        ),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.download),
+                      color: Colors.black,
+                      onPressed: () {
+                        // download functionality
+                      },
+                    ),
                   ),
                 ),
               );
